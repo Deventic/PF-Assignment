8e753274430cec714be854c95109cb26f60d41ff
Fardeen Raza <141150986+Deventic@users.noreply.github.com>
2025-10-30 19:07:57 +0500

    Update 25L_0897_Q8.cpp

--- 25L_0897_Q8.cpp
+++ 25L_0897_Q8.cpp
@@ -1,31 +1,32 @@
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
-// int main() {
-//     int N;
-//     cout << "Enter number of courses: ";
-//     cin >> N;
+int main() {
+    int N;
+    cout << "Enter number of courses: ";
+    cin >> N;
 
-//     double gradePoint, creditHours;
-//     double totalGradePoints = 0, totalCreditHours = 0;
+    double gradePoint, creditHours;
+    double totalGradePoints = 0, totalCreditHours = 0;
 
-//     for (int i = 1; i <= N; i++) {
-//         cout << "\nCourse " << i << ":" << endl;
-//         cout << "Enter grade points: ";
-//         cin >> gradePoint;
-//         cout << "Enter credit hours: ";
-//         cin >> creditHours;
+    for (int i = 1; i <= N; i++) {
+        cout << "\nCourse " << i << ":" << endl;
+        cout << "Enter grade points: ";
+        cin >> gradePoint;
+        cout << "Enter credit hours: ";
+        cin >> creditHours;
 
-//         totalGradePoints += (gradePoint * creditHours);
-//         totalCreditHours += creditHours;
-//     }
+        totalGradePoints += (gradePoint * creditHours);
+        totalCreditHours += creditHours;
+    }
 
-//     if (totalCreditHours == 0) {
-//         cout << "\nInvalid input: total credit hours cannot be zero." << endl;
-//     } else {
-//         double SGPA = totalGradePoints / totalCreditHours;
-//         cout << "\nYour Semester GPA (SGPA) is: " << SGPA << endl;
-//     }
+    if (totalCreditHours == 0) {
+        cout << "\nInvalid input: total credit hours cannot be zero." << endl;
+    } else {
+        double SGPA = totalGradePoints / totalCreditHours;
+        cout << "\nYour Semester GPA (SGPA) is: " << SGPA << endl;
+    }
 
-//     return 0;
-// }
+    return 0;
+}
+
